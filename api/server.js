73f4169b30c66ce9fa0b726070d6ff903299514e
Zobio <soshinito514@gmail.com>
2021-04-25 21:20:09 +0900

    requestモジュールを追加

--- api/server.js
+++ api/server.js
@@ -14,35 +14,22 @@ const app = express();
 app.post('/webhook', line.middleware(config), (req, res) => {
 	console.log(req.body.events);
 
-	//ここのif分はdeveloper consoleの"接続確認"用なので削除して問題ないです。
-	if (req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff') {
-		res.send('Hello LINE BOT!(POST)');
-		console.log('疎通確認用');
-		return;
-	}
+	res.send('Hello LINE BOT!(POST)');
+	console.log('疎通確認用');
 
 	Promise
 		.all(req.body.events.map(handleEvent))
-		.then((result) => res.json(result));
+		//.then((result) => res.json(result));
 });
 
 const client = new line.Client(config);
 
 async function handleEvent(event) {
-	if(true) {
-		return client.replyMessage(event.replyToken, {
-			type: 'text',
-			text: 'TEST'
-		});
-	}
 	if (event.type !== 'message' || event.message.type !== 'text') {
 		return Promise.resolve(null);
 	}
 
-	var path = 'C:/Users/soshi/Desktop/LineBot';
-
-	var nameFunc = require(path + '/api/fix_name.js');
-	var receivedText = nameFunc.fix_name(event.message.text);
+	var receivedText = event.message.text;
 
 	var url = 'https://zobio.github.io/image/pokemon-card/regulation_e/';
 	url += receivedText;
@@ -51,13 +38,15 @@ async function handleEvent(event) {
 	console.log('https://zobio.github.io/image/pokemon-card/regulation_e/' + receivedText + '/1.txt')
 	try{
 		const fs = require('fs');
-		var text = fs.readFileSync(encodeURI(path + '/image/pokemon-card/regulation_e/') + receivedText + '/1.txt', 'utf8');
+		var text = fs.readFileSync('/image/pokemon-card/regulation_e/' + encodeURI(receivedText) + '/1.txt', 'utf8');
+		console.log('text: ' + text);
 		var lines = text.toString().split('¥n');
 		var rep = '';
 		for (var ln of lines) {
 			rep += ln;
 		}
 	}catch(error){
+		console.log(error);
 		return client.replyMessage(event.replyToken, {
 		type: 'text',
 		text: '指定された名前のカードが見つかりませんでした。'
